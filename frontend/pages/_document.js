e4c65ceca2d0a09ef4165f5452d4605188b66795
Bakery Platform <bakery-platform@github.com>
2026-04-20 15:41:46 +0000

    fix: add meta no-cache tags to prevent stale bundle hash in browser

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -4,6 +4,11 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Force browser to never cache HTML pages */}
+        <meta httpEquiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+        <meta httpEquiv="Pragma"        content="no-cache" />
+        <meta httpEquiv="Expires"       content="0" />
+
         <link rel="preconnect" href="https://fonts.googleapis.com" crossOrigin="anonymous" />
         <link rel="preconnect" href="https://fonts.gstatic.com"    crossOrigin="anonymous" />
         <link
